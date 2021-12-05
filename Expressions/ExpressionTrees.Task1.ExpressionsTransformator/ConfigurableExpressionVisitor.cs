582304f7a7ed7861feb07b00c3143706b7b078c2
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2021-12-05 14:43:27 +0300

    Add Lambda expression override Visitor

---
+++ Expressions/ExpressionTrees.Task1.ExpressionsTransformator/ConfigurableExpressionVisitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer
+{
+	public class ConfigurableExpressionVisitor : ExpressionVisitor
+	{
+		private IDictionary<string, object> ParameterReplacementConfig { get; set; }
+
+		/// <summary>
+		///  * 2. changes parameter values in a lambda expression to constants, taking the following as transformation parameters:
+		///  *    - source expression;
+		///  *    - dictionary: 
+		///				parameter name: value for replacement
+		/// </summary>
+		/// <param name="node"></param>
+		/// <param name="config"></param>
+		/// <returns></returns>
+		public Expression ReplaceParamsByConfiguration(Expression node, IDictionary<string, object> config)
+		{
+			this.ParameterReplacementConfig = config;
+
+			return this.Visit(node);
+		}
+
+		protected override Expression VisitLambda<TDelegate>(Expression<TDelegate> node)
+		{
+			bool requiresTransformation = node.Parameters.Select(p => p.Name).Any(this.ParameterReplacementConfig.ContainsKey);
+
+			if (requiresTransformation)
+			{
+				// substitude of an original param with constant value
+				Expression[] arguments = node.Parameters.Select(parameter =>
+					this.ParameterReplacementConfig.TryGetValue(parameter.Name, out object newValue) ?
+					Expression.Constant(newValue, newValue.GetType()) :
+					(Expression)parameter
+				).ToArray();
+
+				Type[] typeArguments = arguments.Select(arg => arg.Type).ToArray();
+
+				MethodInfo methodInfo = node.Type.GetMethod("Invoke");
+				
+				// create MethosdCall expression of Delegate's Invoke method
+				MethodCallExpression newLambdaExpression = Expression.Call(node, methodInfo, arguments);
+
+				// creating a new Labmbda expression of Invoke method call
+				return Expression.Lambda<TDelegate>(newLambdaExpression, node.Parameters);
+			}
+
+			return node;
+		}
+	}
+}
--- Expressions/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ Expressions/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -2,8 +2,64 @@
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
-    public class IncDecExpressionVisitor : ExpressionVisitor
-    {
-        // todo: feel free to add your code here
-    }
+	public class IncDecExpressionVisitor : ExpressionVisitor
+	{
+		public Expression ReduceIncrementDecrementExpressions(Expression node)
+		{
+			return base.Visit(node);
+		}
+
+		/// <summary>
+		/// Converts expressions like <variable> + 1 to increment operations, <variable> - 1 - into decrement operations
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		protected override Expression VisitBinary(BinaryExpression node)
+		{
+			Expression newExpression = null;
+			bool isRightHandConstantNumberOne = node.Right.NodeType == ExpressionType.Constant && this.IsIntegerConstantValue(node.Right, 1);
+
+			switch (node.NodeType)
+			{
+				case ExpressionType.Add:
+					{
+						if (isRightHandConstantNumberOne)
+						{
+							newExpression = Expression.Increment(node.Left);
+						}
+
+						break;
+					}
+				case ExpressionType.Subtract:
+					{
+						if (isRightHandConstantNumberOne)
+						{
+							newExpression = Expression.Decrement(node.Left);
+						}
+
+						break;
+					}
+				default:
+					break;
+			}
+
+			return newExpression ?? node;
+		}
+
+		/// <summary>
+		/// Helper method.
+		/// </summary>
+		/// <param name="node"></param>
+		/// <param name="valueToCompare"></param>
+		/// <returns></returns>
+		private bool IsIntegerConstantValue(Expression node, int valueToCompare)
+		{
+			if (node is ConstantExpression constant && constant.Value is int value)
+			{
+				return value.Equals(valueToCompare);
+			}
+
+			return false;
+		}
+	}
 }
