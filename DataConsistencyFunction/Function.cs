ba7c21ef76db12e8f59d4de69f766d3e4da72369
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2024-10-08 13:28:16 +0400

    refactor(DataConsistencyFunction) response compatible with APIGatewayProxy

--- DataConsistencyFunction/Function.cs
+++ DataConsistencyFunction/Function.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
@@ -18,7 +19,7 @@ var connectionString =
     Environment.GetEnvironmentVariable("Postgres") ??
     throw new InvalidOperationException("Connection string to the Postgres database can't be found.");
 
-var dataSource = NpgsqlDataSource.Create(connectionString);
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
 
 // Ensure table created
 await using var cmd = dataSource.CreateCommand(SqlCommands.CreateIfNotExists);
@@ -27,21 +28,28 @@ await cmd.ExecuteNonQueryAsync();
 // The function handler that will be called for each Lambda event
 // Some basic validation that verifies that DB metadata corresponds to the images in the S3 bucket.
 // The lambda should return a result that shows if data is consistent or not.
-var handler = async (ILambdaContext context) =>
+var handler = async (object request, ILambdaContext context) =>
 {
+    context.Logger.LogInformation($"Executing lambda function from {request}.");
+
     await using var command = dataSource.CreateCommand(SqlCommands.CountRows);
 
     var imagesCount = Convert.ToInt32(await command.ExecuteScalarAsync());
     var objectKeys = await amazonS3.GetAllObjectKeysAsync(bucketName, null, null);
 
-    if (imagesCount == objectKeys.Count)
+    var isConsistent = imagesCount == objectKeys.Count;
+    var responseMessage = isConsistent ? "Data consistency check: PASSED" : "Data consistency check: FAILED";
+
+    if (!isConsistent)
+        context.Logger.LogCritical("The number of images in RDS is different from S3 bucket keys.");
+
+    return new APIGatewayProxyResponse
     {
-        return HttpStatusCode.OK;
-    }
-
-    context.Logger.LogCritical("The number of images in RDS is different from S3 bucket keys.");
-
-    return HttpStatusCode.InternalServerError;
+        IsBase64Encoded = false,
+        StatusCode = (int)HttpStatusCode.OK,
+        Headers = new Dictionary<string, string>(),
+        Body = responseMessage
+    };
 };
 
 // Build the Lambda runtime client passing in the handler to call for each
